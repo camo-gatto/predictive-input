29e93022fd3f1b797bfc8994b91ef7e623f59de2
nicolads87 <nicola.ds87@gmail.com>
2015-06-05 17:56:05 +0200

    predictive

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = angular.module('predictiveInput', []);
+app.controller('predictive', function($scope) {
+    console.log('predictive');
+    $scope.words = ["bientôt", "ciao", "élucidation", "écoute", "écris", "eûmes", "bella", "ragazza", "meteo", "che", "chez", "ché", "gatto", "gattopardo"];
+    
+    $scope.select = function(word) {
+        $scope.text = word + " ";
+    }
+    
+});
+
+app.directive('typing', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        scope: {typingWord: '='},
+        link: function(scope, element, attr, ngModel){
+            
+            ngModel.$parsers.push(function(text){
+                var lastWord = text.slice(text.lastIndexOf(' ') + 1);
+                if(text[text.length -1] == " ") {
+                    scope.typingWord = undefined;    
+                } else {
+                    scope.typingWord = lastWord;
+                }
+            });
+            /*ngModel.$formatters.push(function(value){
+                console.log('$formatters ', value);
+            });*/
+        }
+    
+    }
+});
